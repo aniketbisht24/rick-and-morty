f6e56e0940fd87e2303e3cbf4b41dde5be5c6545
Aniket Bisht <aniketbisht24@gmail.com>
2022-12-07 11:06:54 +0530

    integrated api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Table from './components/Table/table';
 
 function App() {
   return (
     <div className="App">
-      hello
-    
+      <Table />
     </div>
   );
 }
