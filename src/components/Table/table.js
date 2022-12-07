cdb453c9e360170a263a77c93d42b7d9a7c16a7c
Aniket Bisht <aniketbisht24@gmail.com>
2022-12-07 12:00:32 +0530

    project completed

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -6,52 +6,60 @@ const Table = () => {
   const [search, setSearch]=useState('')
   const [data, setData] = useState([])
 
-  const handleChange = (e) =>{
-    e.preventDefault();
-    setSearch(e.target.value)
-  }
   
-  const getData = async() =>{
-    try{
-      const response = await axios({
-        method: 'get',
-        url: 'https://rickandmortyapi.com/api/character',
-        params: {name: search}
-      })
-      setData(response.data.results)
-    }catch(err){
-      setData([])
-    }
-  }
-
   useEffect(() => {
+    const getData = async() =>{
+      try{
+        const response = await axios({
+          method: 'get',
+          url: 'https://rickandmortyapi.com/api/character',
+          params: {name: search}
+        })
+        setData(response.data.results)
+      }catch(err){
+        setData([])
+      }
+    }
     getData();
   }, [search])
   
-  // console.log(data);
   return (
     <div>
       <div className="search-box">
-        <input type="text" onChange={handleChange}/>
+        <label> Search your Favorite Character here </label>
+        <input type="text" placeholder='Enter data to be searched' onChange={(e) => setSearch(e.target.value)} />
       </div>
 
-      {
-        data.length ? data.map((element) => 
-        {
-          return (
-            <div className="tableRow" key={element.id}>
-              {element.name}
-              {element.status}
-              {element.species}
-              {element.gender}
-              <img src={element.image} alt="cartoons"/>
-            </div>
-          )
-        }
-        ) : <span></span>
-      }
       <div className="table-box">
+        {
+          data.length ? data.map((element) => 
+          {
+            return (
+              <div className="tableRow" key={element.id}>
+                <div className="element-image">
+                  <img src={element.image} alt="cartoons"/>
+                </div>
 
+                <div className="right-alignment">
+                  <div className="element-name">
+                    {element.name}
+                  </div>
+                  
+                  <div className="element-status">
+                        {element.status} - {element.species}
+                  </div>
+
+                  <div className="element-gender">
+                    Gender: {element.gender}
+                  </div>
+                </div>
+              </div>
+            )
+          }
+          ) :               <div className="tableEmpty">
+              No element found
+            </div>
+        }
       </div>
     </div>
   )
